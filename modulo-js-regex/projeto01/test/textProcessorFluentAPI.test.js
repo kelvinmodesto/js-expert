348180451432d8feccee23edd488b6cb8f51cb6e
Kelvin Modesto <km.modesto@gmail.com>
2023-12-14 21:58:45 -0300

    Finalizando aula de extração de dados por regex

--- modulo-js-regex/projeto01/test/textProcessorFluentAPI.test.js
+++ modulo-js-regex/projeto01/test/textProcessorFluentAPI.test.js
@@ -9,4 +9,21 @@ describe('TextProcessorFluentAPI suit test', () => {
 
     expect(result).to.be.deep.equal(mock);
   });
+
+  it('#extractPersonData', () => {
+    const result = new TextProcessorAPI(mock).extractPeopleData().build();
+
+    const expected = [
+      [
+        'Luffy D. Monkey, brasileiro, solteiro, CPF 235.743.420-12, residente e ',
+        'domiciliada a Rua dos bobos, zero, bairro Alphaville, São Paulo. ',
+      ].join('\n'),
+      [
+        'Robin Nico, austríaca, solteira, CPF 297.947.800-81, residente e ',
+        'domiciliada a Av. dos Estados, 99, bairro Jardins, São Paulo. ',
+      ].join('\n'),
+    ];
+
+    expect(result).to.be.deep.equal(expected);
+  });
 });
