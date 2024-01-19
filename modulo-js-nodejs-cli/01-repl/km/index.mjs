da2fbe7aaef552485ffad29e1d5512acfe8e7148
Kelvin Modesto <km.modesto@gmail.com>
2024-01-19 18:30:15 -0300

    Finalizando exercício repl

--- modulo-js-nodejs-cli/01-repl/km/index.mjs
+++ modulo-js-nodejs-cli/01-repl/km/index.mjs
@@ -6,3 +6,14 @@ function liquidSalary({ salary, discount }) {
   const cost = salary * (discount / 100);
   return salary - cost;
 }
+
+http
+  .createServer((req, res) => {
+    const url = req.url.replace('/', '');
+    const params = new URLSearchParams(url);
+    const data = Object.fromEntries(params);
+    const result = liquidSalary(data);
+    // debugger
+    res.end(`O seu salario final é: ${result}`);
+  })
+  .listen(3000, () => console.log('Listening at 3000...'));
