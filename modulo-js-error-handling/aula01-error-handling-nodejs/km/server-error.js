8e4f78906fc51a0e565ba4c76ef4ea40e2a4403c
Kelvin Modesto <km.modesto@gmail.com>
2024-01-31 23:08:30 -0300

    Finalizando aula 1 error handling

---
+++ modulo-js-error-handling/aula01-error-handling-nodejs/km/server-error.js
@@ -0,0 +1,33 @@
+import Http from 'http';
+
+let count = 1;
+
+async function handler(request, response) {
+  count++;
+  try {
+    if (count % 2 === 0) {
+      await Promise.reject('error inside handler');
+    }
+
+    for await (const data of request) {
+      try {
+        if (count % 2 !== 0) {
+          await Promise.reject('error inside for/await');
+        }
+      } catch (error) {
+        console.log('a request error has happened', error);
+        response.writeHead(500);
+        response.write(JSON.stringify({ message: 'internal server error' }));
+      } finally {
+        response.end();
+      }
+    }
+  } catch (error) {
+    console.log('a server error has happened', error);
+    response.writeHead(500);
+    response.write(JSON.stringify({ message: 'internal server error' }));
+    response.end();
+  }
+}
+
+Http.createServer(handler).listen(3000, () => console.log('listening at 3000'));
