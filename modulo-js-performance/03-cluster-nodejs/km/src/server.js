b73c435e6d66780bd90a09e926478d99efc7f1f1
Kelvin Modesto <km.modesto@gmail.com>
2024-02-07 18:15:55 -0300

    Finalizando módulo de performance

--- modulo-js-performance/03-cluster-nodejs/km/src/server.js
+++ modulo-js-performance/03-cluster-nodejs/km/src/server.js
@@ -3,9 +3,16 @@ import { appendFile } from 'fs/promises';
 
 export function initializeServer() {
   async function handler(request, response) {
-    response.end();
+    await appendFile('./log.txt', `processed by ${process.pid}\n`);
+
+    const result = Array.from({ length: 1e3 }, (_) =>
+      Math.floor(Math.random() * 50)
+    ).reduce((prev, next) => prev + next, 0);
+
+    response.end(result.toString());
   }
   createServer(handler).listen(3000, () =>
     console.log(`listening at 3000 and pid ${process.pid}`)
   );
+  // setTimeout(() => process.exit(1), Math.random() * 1e4);
 }
